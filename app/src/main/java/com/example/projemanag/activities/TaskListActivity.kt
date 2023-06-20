cdf1170e9ff9ebae0ede52eb304dc6faab20d397
JuanGR-FI <juan60430@hotmail.com>
2023-06-19 19:05:05 -0600

    Adding new task to Board, updating DB

--- app/src/main/java/com/example/projemanag/activities/TaskListActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/TaskListActivity.kt
@@ -13,6 +13,9 @@ import com.example.projemanag.models.Task
 import com.example.projemanag.utils.Constants
 
 class TaskListActivity : BaseActivity() {
+
+    private lateinit var mBoardDetails : Board
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_list)
@@ -26,7 +29,7 @@ class TaskListActivity : BaseActivity() {
         FirestoreClass().getBoardDetails(this, boardDocumentId)
     }
 
-    private fun setupActionBar(title: String){
+    private fun setupActionBar(){
         val toolbarTaskList = findViewById<Toolbar>(R.id.toolbar_task_list_activity)
         setSupportActionBar(toolbarTaskList)
 
@@ -34,7 +37,7 @@ class TaskListActivity : BaseActivity() {
         if(actionBar != null){
             actionBar.setDisplayHomeAsUpEnabled(true)
             actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
-            actionBar.title = title
+            actionBar.title = mBoardDetails.name
         }
 
         toolbarTaskList.setNavigationOnClickListener {
@@ -44,8 +47,10 @@ class TaskListActivity : BaseActivity() {
     }
 
     fun boardDetails(board: Board){
+        mBoardDetails = board
+
         hideProgressDialog()
-        setupActionBar(board.name)
+        setupActionBar()
 
         val addTaskList = Task(resources.getString(R.string.add_list))
         board.taskList.add(addTaskList)
@@ -59,4 +64,21 @@ class TaskListActivity : BaseActivity() {
         rvTaskList.adapter = adapter
 
     }
+
+    fun addUpdateTaskListSuccess(){
+        hideProgressDialog()
+
+        showProgressDialog(resources.getString(R.string.please_wait))
+        FirestoreClass().getBoardDetails(this, mBoardDetails.documentId)
+    }
+
+    fun createTaskList(taskListName: String){
+        val task = Task(taskListName, FirestoreClass().getCurrentUserId())
+        mBoardDetails.taskList.add(0, task)
+        mBoardDetails.taskList.removeAt(mBoardDetails.taskList.size - 1)
+
+        showProgressDialog(resources.getString(R.string.please_wait))
+        FirestoreClass().addUpdateTaskList(this, mBoardDetails)
+    }
+
 }
--- app/src/main/java/com/example/projemanag/firebase/FirestoreClass.kt
+++ app/src/main/java/com/example/projemanag/firebase/FirestoreClass.kt
@@ -38,8 +38,10 @@ class FirestoreClass {
             .addOnSuccessListener {
                     document ->
                 Log.i(activity.javaClass.simpleName, document.toString())
+                val board = document.toObject(Board::class.java)!!
+                board.documentId = document.id
 
-                activity.boardDetails(document.toObject(Board::class.java)!!)
+                activity.boardDetails(board)
 
             }.addOnFailureListener {
                     e ->
@@ -82,6 +84,24 @@ class FirestoreClass {
                 e ->
                 activity.hideProgressDialog()
                 Log.e(activity.javaClass.simpleName, "Error while creating a board.", e)
+            }
+    }
+
+    fun addUpdateTaskList(activity: TaskListActivity, board: Board){
+        val taskListHashMap = HashMap<String, Any>()
+        taskListHashMap[Constants.TASK_LIST] = board.taskList
+
+        mFirestore.collection(Constants.BOARDS)
+            .document(board.documentId)
+            .update(taskListHashMap)
+            .addOnSuccessListener {
+                Log.e(activity.javaClass.simpleName, "TaskList updated successfully")
+                activity.addUpdateTaskListSuccess()
+
+            }.addOnFailureListener {
+                exception ->
+                activity.hideProgressDialog()
+                Log.e(activity.javaClass.simpleName, "Error while loading board", exception)
             }
     }
 
